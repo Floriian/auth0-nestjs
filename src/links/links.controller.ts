0aafa6d0beaa4b367a0ff01d30c4cd66968699ad
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-06 20:27:41 +0200

    basic links.

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { LinksService } from './links.service';
+import { JwtGuard } from '../auth/guards/jwt.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('links')
 export class LinksController {
   constructor(private readonly linksService: LinksService) {}
+
+  @Get()
+  @UseGuards(JwtGuard)
+  getLinks() {
+    return this.linksService.getLinks();
+  }
 }
--- src/links/links.service.ts
+++ src/links/links.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LinksService {}
+export class LinksService {
+  #links: Array<{ baseUrl: string; shortUrl: string }> = [
+    {
+      baseUrl: 'https://google.com',
+      shortUrl: 'sdkflawsjdf',
+    },
+  ];
+
+  getLinks() {
+    return this.#links;
+  }
+}
