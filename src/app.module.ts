1130519a78a018cfe89b091ea0f98b2908d12909
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-06 18:02:47 +0200

    generate links module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { LinksModule } from './links/links.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, LinksModule],
   controllers: [AppController],
   providers: [AppService],
 })
