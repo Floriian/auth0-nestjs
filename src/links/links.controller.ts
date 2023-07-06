cd13c11c0e1825995f31ef87bff55ec186199e76
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-06 21:29:45 +0200

    remove unused imports

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { LinksService } from './links.service';
 import { JwtGuard } from '../auth/guards/jwt.guard';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('links')
 export class LinksController {
