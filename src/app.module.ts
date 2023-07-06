6174ae0fd4ec0fbc210844f82d8a8bb13d7a7953
Lovasi Flórián <lovasiflorian@outlook.com>
2023-07-06 20:52:11 +0200

    remove defualt files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { LinksModule } from './links/links.module';
 
 @Module({
   imports: [AuthModule, LinksModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
